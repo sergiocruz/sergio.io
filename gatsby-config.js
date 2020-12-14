82559af34dc8d5d6cb8ca9a4a5630cc89af31fc6
Sergio Cruz <hello@sergiocruz.me>
2020-12-13 22:57:57 -0600

    Fix manifest and purge

--- gatsby-config.js
+++ gatsby-config.js
@@ -96,18 +96,18 @@ module.exports = {
         ]
       }
     },
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: 'gatsby-starter-default',
-    //     short_name: 'starter',
-    //     start_url: '/',
-    //     background_color: '#663399',
-    //     theme_color: '#663399',
-    //     display: 'minimal-ui',
-    //     icon: 'src/images/sergio-icon.png', // This path is relative to the root of the site.
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/sergio-icon.png', // This path is relative to the root of the site.
+      },
+    },
     {
       resolve: `gatsby-plugin-sass`,
       options: {
@@ -129,6 +129,9 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-purgecss',
       options: {
+        // printRejected: true,
+        // whitelist: [],
+        // develop: true,
         ignore: [
           'node_modules/prismjs/'
         ]
