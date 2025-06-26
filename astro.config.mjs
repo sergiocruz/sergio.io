a19ee85b857856741c4d7c0eff7ee4013ab50454
Sergio Cruz <hello@sergiocruz.me>
2025-06-26 17:28:54 -0500

    Updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from 'tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://sergio.io',
